145228578a390f7657f9d70fd41bd413e51d7376
Mehedi Hasan <mehedi08h@gmail.com>
2022-02-26 12:24:56 +0600

    create product schema

---
+++ models/product.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Please enter product name'],
+        trim: true,
+        maxLength: [100, 'Product name cannot exceed 100 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter product description'],
+    },
+    prices: {
+        type: [Number],
+        required: [true, 'Please enter product price'],
+    },
+    extraOptions: {
+        type: [
+            {
+                text: { type: String, required: [true, 'Please enter extras name'] },
+                price: { type: Number, required: [true, 'Please enter extras price'] },
+            },
+        ],
+    },
+    ratings: {
+        type: Number,
+        default: 0
+    },
+    numOfReviews: {
+        type: Number,
+        default: 0
+    },
+    images: [
+        {
+            public_id: {
+                type: String,
+                required: true
+            },
+            url: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    reviews: [
+        {
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+                required: true
+            },
+            name: {
+                type: String,
+                required: true
+            },
+            rating: {
+                type: Number,
+                required: true
+            },
+            comment: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.models.Room || mongoose.model('Room', productSchema);
