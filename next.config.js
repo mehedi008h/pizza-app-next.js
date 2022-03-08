43ee72b7b586a7cdeccff684c85097694b09601b
Mehedi Hasan <mehedi08h@gmail.com>
2022-03-08 13:15:52 +0600

    change env file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
 module.exports = {
   reactStrictMode: true,
   env: {
-    NEXTAUTH_URL: 'https://pizza-app-eta.vercel.app/',
+    DB_URI:
+      "mongodb+srv://mehedi008h:mehedi@cluster0.t0y2w.mongodb.net/pizza-app?retryWrites=true&w=majority",
+
+    CLOUDINARY_CLOUD_NAME: "mehedi08h",
+    CLOUDINARY_API_KEY: "78988226649835",
+    CLOUDINARY_API_SECRET: "Bq5r1QWlxUUqVTdbb3qJPc22Xu8",
+
+    STRIPE_WEBHOOK_SECRET: "whsec_ym8lhPvZ6BrxL3xkJU8qQw8stCD42N0",
+
+    SMTP_HOST: "smtp.mailtrap.io",
+    SMTP_PORT: "2525",
+    SMTP_USER: "1b70e90247425a",
+    SMTP_PASSWORD: "ae8fb495d83b93",
+    SMTP_FROM_EMAIL: "noreply@shopit.com",
+    SMTP_FROM_NAME: "ShopIT",
+
+    STRIPE_SECRET_KEY:
+      "sk_test_51KN85iIdr2NcIiWH0Qr2fWHKcb7yBP9M1N6tCJat1d57Xi2OLvs0WwTESxPZamcUeVxf6DB3XtTlaItmqurv87Px00MTvnXZ3k",
+    STRIPE_API_KEY:
+      "pk_test_51KN85iIdr2NcIiWHKR9b6xuQX7E2gWa2TjyfOnusxouxuSfZynEsFHkC5JaHIIRVzjwOey68JexeEh5ge8CfMl1d00G4PHnGdk",
+    NEXTAUTH_URL: "https://pizza-app-eta.vercel.app/",
   },
   images: {
-    domains: ['res.cloudinary.com'],
-  }
-}
+    domains: ["res.cloudinary.com"],
+  },
+};
